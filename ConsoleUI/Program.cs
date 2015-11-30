e86c80e7fe044c8564629470c646faa766dd6e81
JustKate <KateVavilova.v@gmail.com>
2015-12-01 02:52:03 +0300

    Laba_5_delegate

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic;
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array;
+            array = new int[4][];
+            array[0] = new int[4] { 5, 33, 2, 1 };
+            array[1] = new int[7] { 8, 5, 4, 9, 22, 5, 3 };
+            array[2] = new int[5] { 88, 3, 5, 4, 7 };
+            array[3] = new int[6] { 9, 4, 9, 99, 7, 6 };
+            string k;
+            
+            Boolean a = true;
+            Console.WriteLine("This is start matrix");
+            Matrix.Show(array);
+            while (a)
+            {
+                Console.WriteLine("Choose your action ");
+                Console.WriteLine(" 1-sort by lines\n 2-sort by Max element\n 3-sort by Min element\n 4-end");
+                k = Console.ReadLine();
+                switch (k)
+                {
+                    case "1":
+                        Sort.SortByString(array, Matrix.SumStrin);
+                        Matrix.Show(array);
+                        break;
+                    case "2":
+                        Sort.SortByString(array, Matrix.SearchMax);
+                        Matrix.Show(array);
+                        break;
+                    case "3":
+                        Sort.SortByString(array, Matrix.SearchMin);
+                        Matrix.Show(array);
+                        break;
+
+                    case "4":
+                        a = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ Logic/Matrix.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public delegate int Collect(int[] a);
+    public class Matrix
+    {
+        public static void Show(int[][] a)
+        {
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a[i].Length; j++)
+                    Console.Write("{0} ", a[i][j]);
+                Console.WriteLine();
+            }
+
+        }
+
+        public static int SumStrin(int[] a)
+        {
+            int sum = 0;
+            for (int j = 0; j < a.Length; j++)
+            {
+                sum = sum + a[j];
+            }
+            return sum;
+        }
+
+
+        public static int SearchMax(int[] a)
+        {
+            int max = 0;
+            for (int j = 0; j < a.Length; j++)
+            {
+                if (a[j] > max)
+                { max = a[j]; }
+            }
+            return max;
+        }
+
+        public static int SearchMin(int[] a)
+        {
+            int min = a[0];
+            for (int j = 0; j < a.Length; j++)
+            {
+                if (a[j] < min)
+                { min = a[j]; }
+            }
+            return min;
+        }
+
+
+    }
+
+    public static class Sort
+    {
+        public static void SortByString(int[][] array, Collect collectBy)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (collectBy(array[j]) < collectBy(array[j + 1]))
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+        public static void SortByMax(int[][] array, Collect collectBy)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (collectBy(array[j]) < collectBy(array[j + 1]))
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+        public static void SortByMin(int[][] array, Collect collectBy)
+        {
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (collectBy(array[j]) > collectBy(array[j + 1]))
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+        public static void Swap(ref int[] a, ref int[] b)
+        {
+            int[] temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+
+}
+
